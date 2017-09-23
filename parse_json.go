5b793f02412d1f13636ceb003c261cf033a6e042
Prashant Khurana <prashant@prashantkhurana.com>
2017-09-23 16:29:45 -0400

    serve json on a server

--- parse_json.go
+++ parse_json.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 var FinalMap = make(map[string][]string)
@@ -13,7 +15,7 @@ func main() {
 	files, _ := ioutil.ReadDir("/Users/PKhurana/code/grafana/backup_grafana_db/")
 
 	for _, f := range files {
-		fmt.Println(f.Name())
+		//fmt.Println(f.Name())
 		res2 := map[string]interface{}{}
 		str2, _ := ioutil.ReadFile("/Users/PKhurana/code/grafana/backup_grafana_db/" + f.Name())
 		json.Unmarshal(str2, &res2)
@@ -21,16 +23,17 @@ func main() {
 		parseMap(res2, dbName.(string))
 	}
 
-	for k, v := range FinalMap {
-		fmt.Println(k, v)
-		fmt.Println()
-		fmt.Println()
-		fmt.Println()
-		fmt.Println()
-		fmt.Println()
-		fmt.Println()
+	//fmt.Println(string(x))
+	// for k, v := range FinalMap {
+	// 	fmt.Println(k, v)
+	// 	fmt.Println()
+	// 	fmt.Println()
+	// 	fmt.Println()
+	// 	fmt.Println()
+	// 	fmt.Println()
+	// 	fmt.Println()
 
-	}
+	// }
 
 	// name := "bidder-09_22_2017.json"
 	// str2, _ := ioutil.ReadFile("/Users/PKhurana/code/grafana/backup_grafana_db/" + name)
@@ -40,13 +43,23 @@ func main() {
 	// parseMap(res2, dbName.(string))
 
 	// fmt.Println(FinalMap)
+	http.HandleFunc("/getGrafanaData", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	x, _ := json.Marshal(FinalMap)
+	fmt.Println(string(x))
+	fmt.Fprintf(w, string(x))
 }
 
 func parseMap(aMap map[string]interface{}, dbName string) {
 	for key, val := range aMap {
-		if key == "title" {
+		if key == "title" || key == "measurement" {
 			//fmt.Println(key, ":", val)
-			FinalMap[dbName] = append(FinalMap[dbName], val.(string))
+			if !strings.Contains(val.(string), "$") {
+				FinalMap[dbName] = append(FinalMap[dbName], val.(string))
+			}
 		}
 		switch val.(type) {
 		case map[string]interface{}:
